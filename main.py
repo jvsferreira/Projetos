b0995188c8778adcb9c9f0305105d2b33e029e44
Victor <124278331+jvsferreira@users.noreply.github.com>
2024-09-25 17:20:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+morse_signals = {"A": "*-",
+                 "B": "-***",
+                 "C": "-*-*",
+                 "D": "-**",
+                 "E": "*",
+                 "F": "**-*",
+                 "G": "--*",
+                 "H": "****",
+                 "I": "**",
+                 "J": "*---",
+                 "K": "-*-",
+                 "L": "*-**",
+                 "M": "--",
+                 "N": "-*",
+                 "O": "---",
+                 "P": "*--*",
+                 "Q": "--*-",
+                 "R": "*-*",
+                 "S": "***",
+                 "T": "-",
+                 "U": "**-",
+                 "V": "***-",
+                 "W": "*--",
+                 "X": "-**-",
+                 "Y": "-*--",
+                 "Z": "--**",
+                 "1": "*----",
+                 "2": "**---",
+                 "3": "***--",
+                 "4": "****-",
+                 "5": "*****",
+                 "6": "-****",
+                 "7": "--***",
+                 "8": "---**",
+                 "9": "----*",
+                 "0": "-----",
+                 " ": "/"}
+
+def encode_morse(text):
+    morse_code = ""
+    for char in text:
+        if char.upper() in morse_signals:
+            morse_code += morse_signals[char.upper()] + " "
+    return morse_code
+
+def decode_morse(morse_code):
+    text = ""
+    for word in morse_code.split():
+        for char, signal in morse_signals.items():
+            if word == signal:
+                text += char
+    return text
+
+if __name__ == "__main__":
+    text = "RONEI CU DE MORSA"
+    morse_code = encode_morse(text)
+    print(f"Text: {text}")
+    print(f"Morse Code: {morse_code}")
+    print(f"Decoded Text: {decode_morse(morse_code)}")
